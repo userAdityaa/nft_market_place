de302bc378f291d7d23830af9b2765a6fdcc2c6c
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-27 23:20:50 +0530

    import: Imported all the necessary dependencies in the .sol file

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
