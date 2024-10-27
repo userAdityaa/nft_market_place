2f6ea708cd781e647b3f1818551ea20657c6dd01
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-27 23:30:11 +0530

    feat: Added the listing constructor for the marketplace

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -5,3 +5,27 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    address payable owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listingPrice = 0.01 ether;
+
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;
+    }
+
+    mapping(uint256 => ListedToken) private isToListedToken;
+}
